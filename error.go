f92b011fabf6febd1d6c17dca8447f6ad9d801c4
cpliakas <cpliakas@gmail.com>
2020-04-19 09:16:02 -0400

    Added common error functions

--- error.go
+++ error.go
@@ -8,6 +8,47 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// ErrMissingArgument returns an error when a required option is empty.
+type ErrMissingArgument string
+
+func (e ErrMissingArgument) Error() string {
+	return fmt.Sprintf("missing required argument: %s", string(e))
+}
+
+// ErrInvalidArgument returns an error when the passed option is not valid.
+type ErrInvalidArgument string
+
+func (e ErrInvalidArgument) Error() string {
+	return fmt.Sprintf("argument not valid: %s", string(e))
+}
+
+// ErrMissingOption returns an error when a required option is empty.
+type ErrMissingOption string
+
+func (e ErrMissingOption) Error() string {
+	return fmt.Sprintf("missing required option: %s", string(e))
+}
+
+// ErrInvalidOption returns an error when the passed option is not valid.
+type ErrInvalidOption string
+
+func (e ErrInvalidOption) Error() string {
+	return fmt.Sprintf("option not valid: %s", string(e))
+}
+
+// ErrNotFound returns an error when the passed resource is not found.
+type ErrNotFound string
+
+func (e ErrNotFound) Error() string {
+	return fmt.Sprintf("%s not found", string(e))
+}
+
+//
+// Although the functions below can be useful for simple commands that either
+// succeed for fail without much happening in-between, using the LeveledLogger
+// with LeveledLogger.FatalIfError can provide a better user experience.
+//
+
 // HandleError either performs a no-op if err is nil or writes the error plus
 // command usage to os.Stderr and exits with a non-zero status otherwise.
 func HandleError(cmd *cobra.Command, err error, prefixes ...string) {
