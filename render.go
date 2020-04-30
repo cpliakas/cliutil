03c965897ac482fc2a78c7555847baa920b5955f
cpliakas <cpliakas@gmail.com>
2020-04-30 10:11:40 -0400

    Added JMESPath filtering to JSON

--- render.go
+++ render.go
@@ -3,6 +3,8 @@ package cliutil
 import (
 	"encoding/json"
 	"fmt"
+
+	jmespath "github.com/jmespath/go-jmespath"
 )
 
 // FormatJSON returns pretty-printed JSON as a string and panics on any
@@ -15,7 +17,27 @@ func FormatJSON(v interface{}) string {
 	return string(b)
 }
 
+// FormatJSONWithFilter applies a JMESPath filter and returns pretty-printed
+// JSON as a string and panics on any marshal errors.
+func FormatJSONWithFilter(v interface{}, filter string) (s string, err error) {
+	if filter != "" {
+		if v, err = jmespath.Search(filter, v); err != nil {
+			return
+		}
+	}
+	s = FormatJSON(v)
+	return
+}
+
 // PrintJSON writes pretty-printed JSON to STDOUT.
 func PrintJSON(v interface{}) {
 	fmt.Println(FormatJSON(v))
+}
+
+// PrintJSONWithFilter applies a JMESPath filter and writes pretty-printed JSON
+// to STDOUT.
+func PrintJSONWithFilter(v interface{}, filter string) error {
+	s, err := FormatJSONWithFilter(v, filter)
+	fmt.Println(s)
+	return err
 }
